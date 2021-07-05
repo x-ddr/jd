8ea48e0d7f96487521d457c6b829f1cfa299714e
bug <xd9722@qq.com>
2021-07-05 11:10:54 +0800

    7.5.1

--- jd_europeancup.js
+++ jd_europeancup.js
@@ -31,7 +31,7 @@ if ($.isNode()) {
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
 message = ""
 $.actid = "901100032442101"
-$.shareuuid = "26136166a0714f5193f4298370b9abe9" //俺的助力码 
+$.shareuuid = "243daaa7bf5147378b5ee3b41c1f8e75" //俺的助力码 
     !(async () => {
         if (!cookiesArr[0]) {
             $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {
